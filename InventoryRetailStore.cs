3d9084f7ab8e32749b43ca5ac7298911ebc9e7eb
DimitrisPatros <patrosd@hotmail.com>
2019-05-11 20:51:53 +0300

    formatted code

--- InventoryRetailStore.cs
+++ InventoryRetailStore.cs
@@ -3,44 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 
 
-namespace SmallStore {
-    class InventoryRetailStore : IStore {
-        public InventoryRetailStore() {
+namespace SmallStore
+{
+    class InventoryRetailStore : IStore
+    {
+        public InventoryRetailStore()
+        {
             Inventory = new List<Product>();
             SoldProducts = new List<Product>();
         }
-       
+
         private List<Product> Inventory { get; set; }
         private List<Product> SoldProducts { get; set; }
 
-        public void Buy(Product product) {
+        public void Buy(Product product)
+        {
             Inventory.Add(product);
         }
 
 
-        public void Sell(Product product) {
+        public void Sell(Product product)
+        {
             bool removedProduct = Inventory.Remove(product);
 
-            if (!removedProduct) {
+            if (!removedProduct)
+            {
                 Console.WriteLine("there is no such product in the inventory");
-            } else {
+            }
+            else
+            {
                 SoldProducts.Add(product);
             }
         }
 
-        public double GetRevenue() {
+        public double GetRevenue()
+        {
             double inventoryValue = Inventory.Sum(p => p.PriceWhenBuy);
             double soldItemsNetProfit = SoldProducts.Sum(p => p.PriceWhenSell - p.PriceWhenBuy);
             return soldItemsNetProfit - inventoryValue;
         }
 
-        public void GetInventory() {
-            foreach (Product p in Inventory) {
+        public void GetInventory()
+        {
+            foreach (Product p in Inventory)
+            {
                 Console.WriteLine(p);
             }
         }
 
-        public void Reset() {
+        public void Reset()
+        {
             Inventory = SoldProducts = new List<Product>();
         }
     }
--- Program.cs
+++ Program.cs
@@ -9,19 +9,16 @@ namespace SmallStore
 
             //Create new Simple Store
             IStore galleryDePate = new SimpleStore();
-            
+
             // Creation of Products 
             Product x1 = new Product(1, "Painting Picasso. Guernica", 100, 1000);
             Product x2 = new Product(2, "Painting Tsarouxis. Naftis A", 200, 2000);
             Product x3 = new Product(3, "Chair. Luis XV", 100, 1000);
             Product x4 = new Product(4, "Vase. Cubic", 500, 5000);
-
+            
             //Samples transactions
             galleryDePate.Buy(x1); galleryDePate.Buy(x3); galleryDePate.Sell(x1);
             Console.WriteLine(galleryDePate.GetRevenue());
-
-
-
 
             InventoryRetailStore invStore = new InventoryRetailStore();
             invStore.Buy(x1); invStore.Buy(x3); invStore.Sell(x1);
@@ -29,10 +26,6 @@ namespace SmallStore
             invStore.Sell(x2);
             invStore.GetInventory();
             Console.WriteLine(invStore.GetRevenue());
-
-            Console.ReadKey();
-
-
-        }
+                    }
     }
 }
