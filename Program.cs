4e08d7173940075759b15ec45aac28e7596d4bc0
DimitrisPatros <patrosd@hotmail.com>
2019-05-12 10:02:53 +0300

    format the main program

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ namespace SmallStore
         static void Main(string[] args)
         {
 
-            //Create new Simple Store
+            //Create new Simple Store and InventoryRetailStore
             IStore galleryDePate = new SimpleStore();
+            InventoryRetailStore invStore = new InventoryRetailStore();
 
             // Creation of Products 
             Product x1 = new Product(1, "Painting Picasso. Guernica", 100, 1000);
@@ -16,16 +17,20 @@ namespace SmallStore
             Product x3 = new Product(3, "Chair. Luis XV", 100, 1000);
             Product x4 = new Product(4, "Vase. Cubic", 500, 5000);
             
-            //Samples transactions
-            galleryDePate.Buy(x1); galleryDePate.Buy(x3); galleryDePate.Sell(x1);
+            //Samples transactions of SimpleStore
+            galleryDePate.Buy(x1);
+            galleryDePate.Buy(x3);
+            galleryDePate.Sell(x1);
             Console.WriteLine(galleryDePate.GetRevenue());
 
-            InventoryRetailStore invStore = new InventoryRetailStore();
-            invStore.Buy(x1); invStore.Buy(x3); invStore.Sell(x1);
+            //Samples transactions of InvStore
+            invStore.Buy(x1);
+            invStore.Buy(x3);
+            invStore.Sell(x1);
             Console.WriteLine(invStore.GetRevenue());
             invStore.Sell(x2);
             invStore.GetInventory();
             Console.WriteLine(invStore.GetRevenue());
-                    }
+        }
     }
 }
